84a09865fd984e31e514951480700160dbb16994
yosuaseptian <ys_jojooo@yahoo.com>
2019-02-12 11:25:46 +0700

    nambah Fungsi Bagi

--- index.js
+++ index.js
@@ -4,13 +4,20 @@ var functionJumlah = (a,b) => {
     return hasil
 }
 var functionKurang = (a,b) => {
-    var hasil = a + b
+    var hasil = a - b
+
+    return hasil
+}
+var functionKali = (a,b) => {
+    var hasil = a * b
 
     return hasil
 }
 
 var x = functionJumlah(4,5)
 var i = functionKurang(8,4)
+var l = functionKali(2,4)
 
 console.log(x)
 console.log(i)
+console.log(l)
