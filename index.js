20ca451e2f7d5ab1b3256cfb02fe556017044324
yosuaseptian <ys_jojooo@yahoo.com>
2019-02-12 11:13:54 +0700

    nambah Function Kurang

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ var functionJumlah = (a,b) => {
 
     return hasil
 }
+var functionKurang = (a,b) => {
+    var hasil = a + b
+
+    return hasil
+}
 
 var x = functionJumlah(4,5)
+var i = functionKurang(8,4)
 
 console.log(x)
+console.log(i)
